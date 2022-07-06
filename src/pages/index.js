b037d5580cb68f93430c679371518b4ee131a279
pmr99 <pradeep991004@gmail.com>
2022-07-07 01:22:55 +0800

    landingPage_outline

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import LandingStats from './indexComponents.js/landingStats';
+import LandingProjSummary from './indexComponents.js/landingProjSummary';
+import LandingCollab from './indexComponents.js/landingCollab';
 
 const Home = () => {
 return (
-	<h1>You can write your home!</h1>
+	<div>
+		<div className = "landing-header">[Insert brief overview w/ mission statement & work we do. 1-2 Sentences only.] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.</div>
+		<div className = "landing-stats">
+		<LandingStats img = "" number = "50" subheader = "Completed Projects"> </LandingStats>
+		<LandingStats img = "" number = "30" subheader = "Majors Represented"> </LandingStats>
+		<LandingStats img = "" number = "5" subheader = "Years of Work Carried Out"> </LandingStats>
+		<LandingStats img = "" number = "30" subheader = "Active Members"> </LandingStats>
+		</div>
+		<div className = "second-header"> What We’ve Been Working On </div>
+		<div clssName = "landing-projSummary">
+		<LandingProjSummary img = "" proj = "Project name" pillar = "Health" text = "This project is lit Yall" view = "put hyperlink here"> </LandingProjSummary>
+		<LandingProjSummary img = "" proj = "Project name" pillar = "Health" text = "This project is lit Yall" view = "put hyperlink here"> </LandingProjSummary>
+		<LandingProjSummary img = "" proj = "Project name" pillar = "Health" text = "This project is lit Yall" view = "put hyperlink here"> </LandingProjSummary>
+		</div>
+		<div className = "second-header"> Let’s Work Together </div>
+		<div className = "landing-collab">
+		<LandingCollab title = "Clients" text = "Please work with us bro" button1 = "Learn More" button2 = "Past Work"></LandingCollab>
+		<LandingCollab title = "Students" text = "Please join our club :)" button1 = "Club Culture" button2 = "Recruitment"></LandingCollab>
+		</div>
+		<div className = "second-header"> Spring 2022 Showcase </div>
+
+
+	</div>
 );
 };
 
