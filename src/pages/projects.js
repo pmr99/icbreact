78fae69ed6d3c2530fa8216fbeaaa7956437b2b2
kellyye-cal <kellyye@berkeley.edu>
2022-07-28 21:20:52 -0700

    added styling to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,29 +2,39 @@ import React from 'react';
 import '../projects.css'
 import FeaturedProj from '../components/projectComponents/featuredProj';
 
+import PhilzPreview from '../assets/projects-philz-preview.png'
+
 const Projects = () => {
 return (
 	<div>
 		<img className="hero-image"></img>
-
 		<h4> Featured Projects </h4>
 		<div className="featured-proj-container">
 			<FeaturedProj name="Philz Coffee"
+						banner={PhilzPreview}
+						alt="image of philz coffee cup"
+						skills="Mobile Design · Branding · Data Analysis"
+						description="Reimaging the in-store experience with customers during the COVID-19 pandemic."
+						url="../pages/projects/philz.js">	
+			</FeaturedProj>
+
+			<FeaturedProj name="Philz Coffee"
+						banner={PhilzPreview}
+						alt="image of philz coffee cup"
 						skills="Mobile Design · Branding · Data Analysis"
 						description="Reimaging the in-store experience with customers during the COVID-19 pandemic.">	
 			</FeaturedProj>
 
 			<FeaturedProj name="Philz Coffee"
+						banner={PhilzPreview}
+						alt="image of philz coffee cup"
 						skills="Mobile Design · Branding · Data Analysis"
 						description="Reimaging the in-store experience with customers during the COVID-19 pandemic.">	
 			</FeaturedProj>
 
 			<FeaturedProj name="Philz Coffee"
-						skills="Mobile Design · Branding · Data Analysis"
-						description="Reimaging the in-store experience with customers during the COVID-19 pandemic.">	
-			</FeaturedProj>
-
-			<FeaturedProj name="Philz Coffee"
+						banner={PhilzPreview}
+						alt="image of philz coffee cup"
 						skills="Mobile Design · Branding · Data Analysis"
 						description="Reimaging the in-store experience with customers during the COVID-19 pandemic.">	
 			</FeaturedProj>
