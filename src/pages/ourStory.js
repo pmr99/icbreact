22394cf37bdd22e0baff0e5b8875438ea63793f2
michelle gantos <michelle.gantos@gmail.com>
2022-08-06 18:57:12 -0700

    Started working on Our Story page and added an assests/images folder to store the pillar icons

--- src/pages/ourStory.js
+++ src/pages/ourStory.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import icbIcon_Health from '../assets/images/icbIcon_Health.png';
+import icbIcon_Poverty from '../assets/images/icbIcon_Poverty.png';
+import icbIcon_Society from '../assets/images/icbIcon_Society.png';
+import icbIcon_Environment from '../assets/images/icbIcon_Environment.png';
+
+
+
+import '../ourStory.css'
+import pillarIcon from '../components/pillarIcon';
+
 //hello
 const OurStory = () => {
 return (
-	<h1>You can write your our story!</h1>
+	<div>
+		<img className="hero-image"></img>
+		<div className='block'> 
+		<h4> Our Mission </h4>
+
+		<h5> Inventing ways to make a better world </h5>
+
+		<p> Our mission as a multidisciplinary organization focuses on 
+		human-centered design. We partner with professors, companies, non-profits, 
+		and any entity for social good on campus and beyond. 
+		Our goal is to develop specific projects utilizing design thinking 
+		to improve or introduce solutions on poverty, health, environment, 
+		and society.</p>
+		
+		<pillarIcon image={icbIcon_Health} label="Health" />
+		<pillarIcon image={icbIcon_Poverty} label="Poverty" />
+		<pillarIcon image={icbIcon_Society} label="Society" />
+		<pillarIcon image={icbIcon_Environment} label="Environment" />
+
+
+
+		</div>
+
+		
+
+		
+	</div>
+	
 );
 };
 
